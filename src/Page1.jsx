3a290ec12b5c18037efda8bfb2461b18f1cefb72
mari107 <62844176+MARI107@users.noreply.github.com>
2021-03-23 22:58:15 +0900

    404ページ

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push("/page1/detailA");
+
   return (
     <div>
       <h1>Pege1ページです</h1>
-      <Link to="/page1/detailA">DatailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DatailA</Link>
       <br />
       <Link to="/page1/detailB">DatailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,19 @@
+
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
+const history = useHistory();
+
+const onClickBack = () => history.goBack();
+
   return (
     <div>
       <h1>Pege1DetailAページです</h1>
+      <button onClick={onClick}>戻る</button>
+      <button>戻る</button>
     </div>
   );
 };
